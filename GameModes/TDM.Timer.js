79c0f506d8ccd1b2c744e9ba27e53fc8b07be83d
SSoDm <163316535+SSoDm@users.noreply.github.com>
2024-07-14 16:15:26 +0700

    Create TDM.Timer.js

---
+++ GameModes/TDM.Timer.js
@@ -0,0 +1,17 @@
+// Библиотека для работы, командных матчей ТДМ
+
+import { GameMode } from 'pixel_combats/room';
+
+// Константы, времении
+const PARAMETER_GAME_LENGTH = 'default_game_mode_length';
+
+// Длительность матча, выборного для игрока 
+export function game_mode_length_seconds() {
+    const length = GameMode.Parameters.GetString(PARAMETER_GAME_LENGTH);
+    switch (length) {
+        case 'Length_КОРОТКАЯ': return 140;; // 1 min
+        case 'Length_СРЕДНЯЯ': return 260; // 2 min
+        case 'Length_ДЛИННАЯ': return 480; // 3 min
+    }
+    return 300;
+}
