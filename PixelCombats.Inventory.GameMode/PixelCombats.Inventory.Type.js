04914026e4d4857b1a327e478b8e4c9e47fb495d
PixelCombats 2: Стрелялки войннушки! <163316535+PixelCombats2@users.noreply.github.com>
2024-07-18 18:31:45 +0700

    Update PixelCombats.Inventory.Type.js

--- PixelCombats.Inventory.GameMode/PixelCombats.Inventory.Type.js
+++ PixelCombats.Inventory.GameMode/PixelCombats.Inventory.Type.js
@@ -1,1 +1,13 @@
-
+// Страница, созданная как ПЛЕВОК, для вашего режима
+- Обыкновенный, плевок:
+Player.ContextedProperties.InventoryType.Value = true;
+Player.ContextedProperties.InventoryType.Value = false;
+- Плевок, только по командам:
+BlueTeam.ContextedProperties.InventoryType.Value = true;
+BlueTeam.ContextedProperties.InventoryType.Value = false;
+RedTeam.ContextedProperties.InventoryType.Value = false;
+RedTeam.ContextedProperties.InventoryType.Value = true;
+// Плевок по ID:
+Player.("ID").ContextedProperties.InventoryType.Value = true;
+Player.("ID").ContextedProperties.InventoryType.Value = false;
+({"InventoryType"})
