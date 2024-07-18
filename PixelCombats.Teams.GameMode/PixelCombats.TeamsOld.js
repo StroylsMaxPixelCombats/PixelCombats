2b33837d2a513ef499944164f7deddb2e2a1df18
PixelCombats 2: Стрелялки войннушки! <163316535+PixelCombats2@users.noreply.github.com>
2024-07-18 14:02:05 +0700

    Update PixelCombats.TeamsOld.js

--- PixelCombats.Teams.GameMode/PixelCombats.TeamsOld.js
+++ PixelCombats.Teams.GameMode/PixelCombats.TeamsOld.js
@@ -1,1 +1,55 @@
+// Библиотека, созданная как для КОМАНДЫ, для режима
+
+- Старые, команды:
+Teams.Add("Blue", "Team/Blue", { b: 1 });
+Teams.Add("Red", "Team/Red", { r: 1 });
+var BlueTeam = Teams.Get("Blue");
+var RedTeam = Teams.Get("Red");
+BlueTeam.Build.BlocksSet.Value = BuildBlocksSet.Blue;
+BlueTeam.Spawns.SpawnPointsGroups.Add(1);
+RedTeam.Build.BlocksSet.Value = BuildBlocksSet.Red;
+RedTaem.Spawns.SpawnPointsGroups.Add(2);
+// Старые, лидерБорды:
+LeaderBoard.PlayerLeaderBoardValues = [
+	{
+		Value: "Kills",
+		DisplayName: "Statistics/Kills",
+		ShortDisplayName: "Statistics/KillsShort"
+	},
+	{
+		Value: "Deaths",
+		DisplayName: "Statistics/Deaths",
+		ShortDisplayName: "Statistics/DeathsShort"
+	},
+	{
+		Value: "Spawns",
+		DisplayName: "Statistics/Spawns",
+		ShortDisplayName: "Statistics/SpawnsShort"
+	},
+	{
+		Value: "Scores",
+		DisplayName: "Statistics/Scores",
+		ShortDisplayName: "Statistics/ScoresShort"
+	}
+];
+LeaderBoard.TeamLeaderBoardValue = {
+	Value: "Deaths",
+	DisplayName: "Statistics\Deaths",
+	ShortDisplayName: "Statistics\Deaths"
+};
+// Var = var.GetContext();
+["Var"] - "Вар";
+// Color = color.GetContext();
+Color.Blue = B.GetContext();
+B = 1;
+B: 1;
+({ B: 1 });
+({ b: 1 });
+b = B.Team.Color();
+1 = b;
+b.Team.Blue;
+b.TeamColor = Team.Get("Blue");
+
+
+
 
