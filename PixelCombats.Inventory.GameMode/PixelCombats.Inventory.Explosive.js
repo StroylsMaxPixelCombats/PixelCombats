418009017ba7f9fe3ac0aa568b25345646f3a738
PixelCombats 2: Стрелялки войннушки! <163316535+PixelCombats2@users.noreply.github.com>
2024-07-18 14:31:49 +0700

    Update PixelCombats.Inventory.Explosive.js

--- PixelCombats.Inventory.GameMode/PixelCombats.Inventory.Explosive.js
+++ PixelCombats.Inventory.GameMode/PixelCombats.Inventory.Explosive.js
@@ -1,1 +1,26 @@
+// Страница, созданная как делать, гранаты для режима 
 
+- Обычный код, для гранат:
+Player.Inventory.Explosive.Value = true;
+Player.Inventory.Explosive.Value = false;
+// Основное оружие, по командам:
+BlueTeam.Inventory.Explosive.Value = true;
+BlueTeam.Inventory.Explosive.Value = false;
+RedTeam.Inventory.Explosive.Value = false;
+RedTeam.Inventory.Explosive.Value = true;
+- Гранаты, по ID:
+Player.("ID").Inventory.Explosive.Value = true;
+Player.("ID").Inventory.Explosive.Value = false;
+// Бесконечные патроны, на гранаты:
+Player.Inventory.ExplosiveInfinity.Value = true;
+Player.Inventory.ExplosiveInfinity.Value = false;
+} else {
+BlueTeam.Inventory.ExplosiveInfinity.Value = true;
+BlueTeam.Inventory.ExplosiveInfinity.Value = false;
+RedTeam.Inventory.ExplosiveInfinity.Value = false;
+RedTeam.Inventory.ExplosiveInfinity.Value = true;
+// Бесконечные патроны, на гранаты по ID:
+Player.("ID").Inventory.ExplosiveInfinity.Value = true;
+Player.("ID").Inventory.ExplosiveInfinity.Value = false;
+{"Explosive"}
+{"ExplosiveInfinity"};
