e882005fba26da08c7e289dc8ac90e0be5ba7d84
PixelCombats 2: Стрелялки войннушки! <163316535+PixelCombats2@users.noreply.github.com>
2024-07-18 18:55:16 +0700

    Update CARTURE.Timer.js

--- GameModes/CARTURE.Timer.js
+++ GameModes/CARTURE.Timer.js
@@ -9,7 +9,7 @@ var PARAMETER_GAME_LENGTH = 'default_game_mode_length';
 export function game_mode_length_seconds() {
     var length = GameMode.Parameters.GetString(PARAMETER_GAME_LENGTH);
     switch (length) {
-        case 'Length_КОРОТКАЯ': return 140;; // 1 min
+        case 'Length_КОРОТКАЯ': return 140; // 1 min
         case 'Length_СРЕДНЯЯ': return 260; // 2 min
         case 'Length_ДЛИННАЯ': return 480; // 3 min
     }
