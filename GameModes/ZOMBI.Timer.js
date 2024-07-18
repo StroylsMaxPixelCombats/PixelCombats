4d5033490586851a0eea5e57b3858bfc1168a37f
PixelCombats 2: Стрелялки войннушки! <163316535+PixelCombats2@users.noreply.github.com>
2024-07-18 18:54:30 +0700

    Update ZOMBI.Timer.js

--- GameModes/ZOMBI.Timer.js
+++ GameModes/ZOMBI.Timer.js
@@ -1,1 +1,17 @@
+// Библиотека для работы, командных матчей ЗОМБИ
 
+import { GameMode } from 'pixel_combats/room';
+
+// Константы, времении
+var PARAMETER_GAME_LENGTH = 'default_game_mode_length';
+
+// Длительность матча, выборного для игрока 
+export function game_mode_length_seconds() {
+    var length = GameMode.Parameters.GetString(PARAMETER_GAME_LENGTH);
+    switch (length) {
+        case 'Length_КОРОТКАЯ': return 140; // 1 min
+        case 'Length_СРЕДНЯЯ': return 260; // 2 min
+        case 'Length_ДЛИННАЯ': return 480; // 3 min
+    }
+    return 300;
+}
