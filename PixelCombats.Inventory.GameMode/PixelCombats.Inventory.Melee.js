127d8bb550fa3b8368f7e69919303ac404cfc07b
PixelCombats 2: Стрелялки войннушки! <163316535+PixelCombats2@users.noreply.github.com>
2024-07-18 14:27:12 +0700

    Update PixelCombats.Inventory.Melee.js

--- PixelCombats.Inventory.GameMode/PixelCombats.Inventory.Melee.js
+++ PixelCombats.Inventory.GameMode/PixelCombats.Inventory.Melee.js
@@ -1,1 +1,15 @@
+// Страница, созданная как делать, нож для режима 
 
+- Обычный код, для ножа:
+Player.Inventory.Melee.Value = true;
+Player.Inventory.Melee.Value = false;
+// Нож, по командам:
+BlueTeam.Inventory.Melee.Value = true;
+BlueTeam.Inventory.Melee.Value = false;
+RedTeam.Inventory.Melee.Value = false;
+RedTeam.Inventory.Melee.Value = true;
+// Нож, по ID:
+Player.("ID").Inventory.Melee.Value = true;
+Player.("ID").Inventory.Melee.Value = false;
+{"Melee"}
+{"MeleeInfinity"};
